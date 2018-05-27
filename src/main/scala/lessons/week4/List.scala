80bf24df6ae08fdfeb101cadfcb221439a9d8412
Gianfranco Yosida Vilchez <gianfranco.yosida@gmail.com>
2018-05-27 18:42:35 -0300

    covariance, contravariance and invariance

--- src/main/scala/lessons/week4/List.scala
+++ src/main/scala/lessons/week4/List.scala
@@ -1,41 +1,44 @@
+package lessons.week4
+
+import lessons.week3.{Empty, IntSet, NonEmpty}
+// Covariance, contravariance and invariance
+
 // List implementation from week 3
-trait List[T] {
 
+// Make T covariance by prefixing +
+trait List[+T] {
   def isEmpty: Boolean
-
   def head: T
-
   def tail: List[T]
 
+  // make possible the contravariance of T by
+  // bounding the elem to be a supertype of T
+  def prepend[U >: T](elem: U): List[U] = new Cons[U](elem, this)
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
-
   override def isEmpty: Boolean = false
-
 }
 
-class Nil[T] extends List[T] {
-
+object Nil extends List[Nothing] {
   override def isEmpty: Boolean = true
-
-  override def head: T = throw new NoSuchElementException("Nil.head")
-
-  override def tail: List[T] = throw new NoSuchElementException("Nil.tail")
-
+  override def head: Nothing = throw new NoSuchElementException("Nil.head")
+  override def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
 // create a List in the form List(), List(1), List(1,2)
 object List {
+  def apply[T](x: T, y: T): List[T] = new Cons(x, new Cons(y, Nil))
+  def apply[T](x: T): List[T] = new Cons(x, Nil)
+  def apply[T](): List[T] = Nil
 
-  def apply[T](x: T, y: T): List[T] = new Cons(x, new Cons(y, new Nil))
-
-  def apply[T](x: T): List[T] = new Cons(x, new Nil)
-
-  def apply[T](): List[T] = new Nil
-
+  // returns a list of IntSet since Empty is a IntSet and
+  // satisfies the bounding
+  def f(xs: List[NonEmpty]): List[IntSet] = xs prepend Empty
 }
 
-List()
-List(1)
-List(1, 2)
+//List()
+//List(1)
+//List(1, 2)
+
+//val strings: List[String] = Nil //covariance
